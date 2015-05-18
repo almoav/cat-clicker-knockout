5324a5d281ea39210da4f54975205ef7c863e30d
Alex <almoav@gmail.com>
2015-05-18 12:31:58 -0700

    organize by MVO

--- js/main.js
+++ js/main.js
@@ -1,61 +1,150 @@
-// the source cat pics
-var catPics = ['derpy.jpg', 'furpy.jpg', 'lerpy.jpg', 'smurpy.jpg', 'ted.jpg'];
+$(function(){
+	var model = {
+        init: function() {
+            localStorage.cats = JSON.stringify([]);
+            localStorage.currentCat;
+            localStorage.catIndex = 0;
+        },
 
-// declare variables here outside of the loop
-var picCount = catPics.length;
-var catPic, catName, catElem, catCounter;
+        add: function(obj) {
+            var data = JSON.parse(localStorage.cats);
+            data.push(obj);
+            localStorage.cats = JSON.stringify(data)
+        },
 
-// save the DOM elements we are interested in
-var catContainer = document.getElementById('cat_container');
-var catCounter = document.getElementById('counter');
-var catViewer = document.getElementById('cat_viewer');
-var catMeow = document.getElementById('meow');
+        getAllCats: function() {
+            return JSON.parse(localStorage.cats);
+        },
 
-for (i=0; i<picCount; i++) {
-	// format the cat strings nicely
-	catPic = catPics[i];
-	catName = catPic.replace('.jpg', '');
+        setCat: function(obj) {
+        	var data = JSON.parse(localStorage.cats);
+        	data[localStorage.catIndex] = obj;
+        	localStorage.cats = JSON.stringify(data)
+        },
 
-	// create a cat DOM element for each cat pic
-	catElem = document.createElement('img');
-	catElem.id = catName;
-    catElem.className = 'cat';
-    catElem.src = "img/"+catPic;
-    catElem.style.width = "100px";
-    catElem.count = 0;
+        setIndex: function(n) {
+        	localStorage.catIndex = n;
+        },
 
-	// cat's displayed name text
-	catText = document.createElement('span');
-	catText.width = "100px";
-	catText.innerHTML = catName;
+        getCat: function() {
+        	var data = JSON.parse(localStorage.cats);
+        	return data[localStorage.catIndex];
+        }
+    };
 
-	// div containing the cat img and text
-	catBox = document.createElement('div');
-	catBox.className = 'cat-box';
-	catBox.appendChild(catText);
-	catBox.appendChild(catElem);
-	catContainer.appendChild(catBox);
+    var octopus = {
+        addAllCats: function() {
+			// the source cat pics
+			var catPics = ['derpy.jpg', 'furpy.jpg', 'lerpy.jpg', 'smurpy.jpg', 'ted.jpg'];
 
-	// when a cat is selected put its image in the cat viewer
-	catElem.addEventListener('click', function() {
+			// declare variables here outside of the loop
+			var picCount = catPics.length;
+			var catPic, catName, catElem, catCounter;
 
-		catName = this.id;
-		// set the cat viewer's image to this cat's image
-		catViewer.src = this.src;
-		// save the current object in the cat viewer
-		catViewer.currentCat = this;
-		// update the displayed count when switching cats
-		catCounter.innerHTML = catName + ' count: ' + this.count;
-		}, false);
-};
+			for (i=0; i<picCount; i++) {
+				// format the cat strings nicely
+				catPic = catPics[i];
+				catName = catPic.replace('.jpg', '');
+				// add to local storage
+				model.add({
+					pic : catPic,
+					name : catName,
+					count : 0
+				});
+			}
+            listView.render();
+        },
 
-//Cat Viewer is the large cat pic, when the user clicks add to the cat's
-//click count and display
-catViewer.addEventListener('click', function() {
-	// iterate this cat object's count
-	this.currentCat.count++;
-	// update the count display
-	catCounter.innerHTML = this.currentCat.id + ' count: ' + this.currentCat.count;
-	// play a cat noise, not annoying at all
-	catMeow.play();
-});
+        getCats: function() {
+            return model.getAllCats();
+        },
+
+        getCurrentCat: function() {
+        	return model.getCat();
+        },
+
+        setCurrentCat: function(id) {
+        	catList = model.getAllCats();
+        	listLen = catList.length;
+        	for (i=0; i<listLen; i++) {
+        		if ( catList[i].name === id) {
+        			model.setIndex(i);
+        			break;
+        		};
+        	}
+        	clickView.render();
+        },
+
+        clickCat: function() {
+        	cat = model.getCat();
+        	cat.count++;
+        	model.setCat(cat);
+        	clickView.render();
+        },
+
+        init: function() {
+            model.init();
+            listView.init();
+            octopus.addAllCats();
+            octopus.setCurrentCat();
+            clickView.init();
+        }
+    };
+
+    var listView = {
+        init: function() {
+            listView.render();
+        },
+        render: function(){
+			// save the DOM elements we are interested in
+			var catContainer = document.getElementById('cat_container');
+
+        	octopus.getCats().forEach(function(cat){
+
+				// create a cat DOM element for each cat pic
+				catElem = document.createElement('img');
+				catElem.id = cat.name;
+			    catElem.className = 'cat';
+			    catElem.src = "img/"+cat.pic;
+
+				// cat's displayed name text
+				catText = document.createElement('span');
+				catText.className = 'cat-span';
+				catText.innerHTML = cat.name;
+
+				// div containing the cat img and text
+				catBox = document.createElement('div');
+				catBox.className = 'cat-box';
+				catBox.appendChild(catText);
+				catBox.appendChild(catElem);
+				catContainer.appendChild(catBox);
+
+				catElem.addEventListener('click', function() {
+					octopus.setCurrentCat(this.id);
+				});
+        	});
+        }
+    };
+
+    var clickView = {
+    	init: function() {
+			var catCounter = document.getElementById('counter');
+			var catViewer = document.getElementById('cat_viewer');
+    		currentCat = octopus.getCurrentCat();
+    		clickView.render();
+    		catViewer.addEventListener('click', function() {
+    			octopus.clickCat();
+    		});
+    	},
+    	render: function() {
+			var catCounter = document.getElementById('counter');
+			var catViewer = document.getElementById('cat_viewer');
+    		currentCat = octopus.getCurrentCat();
+
+    		catCounter.innerHTML = currentCat.name + ' count: ' + currentCat.count;
+    		catViewer.src = 'img/' + currentCat.pic;
+    	},
+    };
+
+    octopus.init();
+})
