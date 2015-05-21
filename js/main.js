c87e075393eb3627c3902bbf1843569ce9a7e491
Alex <almoav@gmail.com>
2015-05-20 23:15:46 -0700

    add nicknames using foreach on an observable array

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@ var AppViewModel = function() {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Derpy');
     this.imgSrc = ko.observable('img/derpy.jpg');
+    this.nicknames = ko.observableArray([
+        {name: 'Mustard'},
+        {name: 'Tiger'},
+        {name: 'Dirty'},
+        {name: 'Burger'}
+    ]);
+
     this.level = ko.computed(function() {
         if (this.clickCount() > 25) {
             return 'teen';
